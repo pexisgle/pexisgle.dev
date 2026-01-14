7f8eb663144f3939ab3957379ce34eb00cd00af6
pexisgle <248136655+pexisgle@users.noreply.github.com>
2026-01-14 14:37:56 +0000

    style: auto format

--- src/worker-configuration.d.ts
+++ src/worker-configuration.d.ts
@@ -9283,7 +9283,7 @@ type AIGatewayHeaders = {
 	[key: string]: string | number | boolean | object;
 };
 type AIGatewayUniversalRequest = {
-	provider: AIGatewayProviders | string;  
+	provider: AIGatewayProviders | string;
 	endpoint: string;
 	headers: Partial<AIGatewayHeaders>;
 	query: unknown;
@@ -9300,7 +9300,7 @@ declare abstract class AiGateway {
 			extraHeaders?: object;
 		}
 	): Promise<Response>;
-	getUrl(provider?: AIGatewayProviders | string): Promise<string>;  
+	getUrl(provider?: AIGatewayProviders | string): Promise<string>;
 }
 interface AutoRAGInternalError extends Error {}
 interface AutoRAGNotFoundError extends Error {}
